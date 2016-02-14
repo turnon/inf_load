e8c6dd2f1317067efc4eb5098bcdab180e96b4a0
ken <block24block@gmail.com>
2016-02-15 00:02:18 +0800

    add baidu tieba posts page

--- infinitely_load.js
+++ infinitely_load.js
@@ -4,9 +4,10 @@ function selectors() {
         gamersky: [".page_css b", ".Mid2L_con"],
         wallpaperscraft: [".page_active", ".center", 10],
         yinyuetai: [function($, doc){return $(doc).find(".page-nav span").filter(function(){return $(this).attr('class') == null})}, ".mv_list,.page-nav"],
+        "tieba\.baidu\.com\/p": ['.pb_footer .tP', '.pb_content,.pb_footer'],
     }
 
-    var url = window.location.host;
+    var url = window.location.href;
     var selector = null;
 
     for (var pattern in supported) {
