ca0abb1c651483e1c3f14ea5b96d070d1ad462be
ken <block24block@gmail.com>
2016-02-15 16:56:48 +0800

    content scripts are isolated, so no need to pass jQuery into function

--- infinitely_load.js
+++ infinitely_load.js
@@ -3,7 +3,7 @@ function selectors() {
     var supported = {
         gamersky: [".page_css b", ".Mid2L_con"],
         wallpaperscraft: [".page_active", ".center", 10],
-        yinyuetai: [function($, doc){return $(doc).find(".page-nav span").filter(function(){return $(this).attr('class') == null})}, ".mv_list,.page-nav"],
+        yinyuetai: [function(doc){return $(doc).find(".page-nav span").filter(function(){return $(this).attr('class') == null})}, ".mv_list,.page-nav"],
         "tieba\.baidu\.com\/p": ['.pb_footer .tP', '.pb_content,.pb_footer'],
         ituring: ['.PagedList-currentPage', '#mainbar', 5],
         "epubit\.com\.cn\/book": ['.PagedList-currentPage', '.col-md-9 .panel', 5],
@@ -24,45 +24,37 @@ function selectors() {
     return selector;
 }
 
-function work_with($){
+function loadp(cur_p, content, limit){
 
-    function loadp(cur_p, content, limit){
+    // hold the continuously coming content
+    var $place = $(content).parent();
 
-        // hold the continuously coming content
-        var $place = $(content).parent();
+    limit = (limit || 99);
 
-        limit = (limit || 99);
-
-        function next_link(doc){
-            if(--limit == 0) return;
-            var $nex_p = ((typeof cur_p == "function") ? cur_p($, doc) : $(doc).find(cur_p)).next();
-            var link = ($nex_p.is('a') ? $nex_p : $nex_p.find('a')).attr('href');
-            console.log(link);
-            return link;
-        }
-
-        function process_page(data){
-            $place.append($(data).find(content));
-            _loadp(next_link(data));
-        }
-
-        function _loadp(link){
-            link && $.get(link, process_page);
-        }
-
-        _loadp(next_link(document));
-
+    function next_link(doc){
+        if(--limit == 0) return;
+        var $nex_p = ((typeof cur_p == "function") ? cur_p(doc) : $(doc).find(cur_p)).next();
+        var link = ($nex_p.is('a') ? $nex_p : $nex_p.find('a')).attr('href');
+        console.log(link);
+        return link;
     }
 
-    var slt = selectors();
-
-    if(slt){
-        loadp.apply(null, slt);
+    function process_page(data){
+        $place.append($(data).find(content));
+        _loadp(next_link(data));
     }
+
+    function _loadp(link){
+        link && $.get(link, process_page);
+    }
+
+    _loadp(next_link(document));
 
 }
 
-jQuery(document).ready(function(){
+var slt = selectors();
+
+slt && $(document).ready(function(){
     console.log('ready');
-    work_with(jQuery);
+    loadp.apply(null, slt);
 });
