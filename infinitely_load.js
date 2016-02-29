3dfab29a3fd720d533d6c22ad96ba02c2d028716
ken <block24block@gmail.com>
2016-02-29 23:40:20 +0800

    add ituring giftportal & output how many elements fetched

--- infinitely_load.js
+++ infinitely_load.js
@@ -5,7 +5,8 @@ function selectors() {
         wallpaperscraft: [".page_active", ".center", 10],
         yinyuetai: [function(doc){return $(doc).find(".page-nav span").filter(function(){return $(this).attr('class') == null})}, ".mv_list,.page-nav"],
         "tieba\.baidu\.com\/p": ['.pb_footer .tP', '.pb_content,.pb_footer'],
-        ituring: ['.PagedList-currentPage', '#mainbar', 5],
+        "ituring\.com\.cn\/book": ['.PagedList-currentPage', '#mainbar', 5],
+        "ituring\.com\.cn\/users\/giftportal": ['.PagedList-currentPage', 'tr:not(:first)'],
         "epubit\.com\.cn\/book": ['.PagedList-currentPage', '.col-md-9 .panel', 5],
     }
 
@@ -40,7 +41,7 @@ function loadp(cur_p, content, limit){
 
     function process_page(data){
         var $cnt = $(data).find(content);
-        console.log($cnt);
+        console.log($cnt, $cnt.length);
         $tail.after($cnt);
         $tail = $cnt.last();
         _loadp(next_link(data));
