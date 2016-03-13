5bf3622c20eab2250393cca94b4f73ceb6263a4e
ken <block24block@gmail.com>
2016-03-13 17:20:57 +0800

    extract selectors config

--- infinitely_load.js
+++ infinitely_load.js
@@ -1,15 +1,5 @@
 function selectors() {
 
-    var supported = {
-        gamersky: [".page_css b", ".Mid2L_con,.MidL_con"],
-        wallpaperscraft: [".page_active", ".center", 10],
-        yinyuetai: [function(doc){return $(doc).find(".page-nav span").filter(function(){return $(this).attr('class') == null})}, ".mv_list,.page-nav"],
-        "tieba\.baidu\.com\/p": ['.pb_footer .tP', '.pb_content,.pb_footer'],
-        "ituring\.com\.cn\/book": ['.PagedList-currentPage', '#mainbar', 5],
-        "ituring\.com\.cn\/users\/giftportal": ['.PagedList-currentPage', 'tr:not(:first)'],
-        "epubit\.com\.cn\/book": ['.PagedList-currentPage', '.col-md-9 .panel', 5],
-    }
-
     var url = window.location.href;
     var selector = null;
 
