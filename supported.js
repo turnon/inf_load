d1614921d46ac7c7809b938e3bbb9ddf4e8134ab
ken <block24block@gmail.com>
2016-03-20 13:24:16 +0800

    wallpaperscraft layout change

--- supported.js
+++ supported.js
@@ -1,7 +1,7 @@
 var supported = {
 
 gamersky: [".page_css b", ".Mid2L_con,.MidL_con"],
-wallpaperscraft: [".page_active", ".center", 10],
+wallpaperscraft: [function(doc){return $(doc).find('.pager__item_selected').filter(function(){return $.isNumeric($(this).text().trim());})}, ".content,.gui-row,.pager", 10],
 yinyuetai: [function(doc){return $(doc).find(".page-nav span").filter(function(){return $(this).attr('class') == null})}, ".mv_list,.page-nav"],
 "tieba\.baidu\.com\/p": ['.pb_footer .tP', '.pb_content,.pb_footer'],
 "ituring\.com\.cn\/book": ['.PagedList-currentPage', '#mainbar', 5],
